62d16c6ac9935b2e11c4bba27602cf039b123727
prof.toure <hodari@hiddengeniusproject.org>
2018-11-01 18:32:59 -0700

    merge2

--- app/models/event.rb
+++ app/models/event.rb
@@ -6,8 +6,8 @@ belongs_to :user
 validates :location, :address, :city, :state, :latitude, :longitude, presence: true
 
 
-geocoded_by :full_address
-after_validation :geocode, if: ->(obj){ obj.address.present? and obj.address_changed? }
+# geocoded_by :full_address
+# after_validation :geocode, if: ->(obj){ obj.address.present? and obj.address_changed? }
 
 def full_address
  [street, city, state].compact.join(', ')
